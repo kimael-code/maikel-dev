836392a57102a22d547ddbeb70a28ac64a177cda
Maikel Carballo <mcarballo@tutanota.com>
2025-10-14 19:19:39 -0400

    Revert "WIP fix: solving translation reactivity"
    This reverts commit 0afd50fea2f3afc8a05e9b2f702951068fc4dc0d.

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import * as locales from '@nuxt/ui/locale'
+// import * as locales from '@nuxt/ui/locale'
 
 const { locale, t } = useI18n()
 const colorMode = useColorMode()
@@ -36,7 +36,8 @@ watch(locale, async (newLocale) => {
 </script>
 
 <template>
-  <UApp :locale="locales[locale]">
+  <!-- <UApp :locale="locales[locale]"> -->
+  <UApp>
     <NuxtLayout>
       <UMain class="relative">
         <NuxtPage />
